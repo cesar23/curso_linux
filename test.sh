20e80bf8c5568ecb0d09c59110c6b814d3088550
Cesar Auris <perucaos@gmail.com.com>
2022-10-21 14:47:48 -0500

    Cesar@DESKTOP-QP2K89L se actualizo :2022-10-21_14:47:46

--- test.sh
+++ test.sh
@@ -1,6 +1,31 @@
+echo '' > delete_me.sh
+echo 'currentscript="$0"'  >> delete_me.sh
+echo '' >>delete_me.sh
+echo '# Function that is called when the script exits:' >>delete_me.sh
+echo 'function finish {' >>delete_me.sh
+echo 'echo $(date +'%Y-%m-%d_%H-%M-%S')' >>delete_me.sh 
+echo 'echo $(date +'%Y-%m-%d_%H-%M-%S') > salida.txt' >>delete_me.sh
+echo '# shred -u  ::: esto es un comando para  eliminado pero se obiara si no esta instalado' >>delete_me.sh
+echo '#echo "Securely shredding ${currentscript}"; shred -u ${currentscript};' >>delete_me.sh
+echo 'echo "Securely shredding ${currentscript}";' >>delete_me.sh 
+echo 'sleep 3' >>delete_me.sh
+echo '}' >>delete_me.sh
+echo '' >>delete_me.sh
+echo '# When your script is finished, exit with a call to the function, "finish":' >>delete_me.sh
+echo 'trap finish EXIT' >>delete_me.sh
+echo 'rm --  "$( readlink -f -- "${BASH_SOURCE[0]:-$0}" 2> '/dev/null'; )";' >>delete_me.sh
 
- PATH_REPO="/drives/c/repos/curso_docker/cesar/ff"
- result=$(echo "${PATH_REPO}" | sed -r -e 's/^\/drives\/([a-zA-Z])+\/(.*)/\/\1\/\2/' )
- result=$(echo "${result}" | sed "s/\//\\\\/g")
- result=$(echo "${result}" | sed -r -e 's/^\\([a-zA-Z])+\\(.*)$/\1:\\\2/' )
-echo $result
+
+echo 'server { ' > /etc/nginx/sites-available/cesar.peru.com.conf
+echo '        listen 80; '>> /etc/nginx/sites-available/cesar.peru.com.conf
+echo '        listen [::]:80; '>> /etc/nginx/sites-available/cesar.peru.com.conf
+echo ' '>> /etc/nginx/sites-available/cesar.peru.com.conf
+echo '        root /var/www/cesar.peru.com/html; '>> /etc/nginx/sites-available/cesar.peru.com.conf
+echo '        index index.html index.htm index.nginx-debian.html; '>> /etc/nginx/sites-available/cesar.peru.com.conf
+echo ' '>> /etc/nginx/sites-available/cesar.peru.com.conf
+echo '        server_name cesar.peru.com www.cesar.peru.com; '>> /etc/nginx/sites-available/cesar.peru.com.conf
+echo ' '>> /etc/nginx/sites-available/cesar.peru.com.conf
+echo '        location / { '>> /etc/nginx/sites-available/cesar.peru.com.conf
+echo '                try_files $uri $uri/ =404; '>> /etc/nginx/sites-available/cesar.peru.com.conf
+echo '        } '>> /etc/nginx/sites-available/cesar.peru.com.conf
+echo '} '>> /etc/nginx/sites-available/cesar.peru.com.conf
--- xcomandos_standar/comando_string_manejos.md
+++ xcomandos_standar/comando_string_manejos.md
@@ -24,13 +24,52 @@ echo "I want to learn $myVar from this site"
 #### OPCION: 1 - puede fallar si hay codigo script shell
 ```shell
 #cat > file_2.txt <<EOF
-cat > ~/files_salida.txt <<EOF
+cat > file.txt <<EOF
 
-info code info
-info code info
-info code info
-info code info
+echo currentscript="/usr/bin/bash"  >> delete_me.sh
+echo  >> delete_me.sh
+# Function that is called when the script exits:
+function finish {
+echo 2022-10-21_13-03-45
+echo 2022-10-21_13-03-45 > salida.txt
+# shred -u  ::: esto es un comando para  eliminado pero se obiara si no esta instalado
+#echo "Securely shredding "; shred -u ;
+echo "Securely shredding ";
+sleep 3
+}
 
+# When your script is finished, exit with a call to the function, "finish":
+trap finish EXIT
+rm --  "/usr/bin/bash";
+
+
+EOF
+
+# ---------------------- ejemplo N 2
+# :::::: escribe
+cat << EOF > file.txt
+echo currentscript="$0"  >> delete_me.sh
+echo  >> delete_me.sh
+# Function that is called when the script exits:
+function finish {
+echo $(date +'%Y-%m-%d_%H-%M-%S')
+echo $(date +'%Y-%m-%d_%H-%M-%S') > salida.txt
+# shred -u  ::: esto es un comando para  eliminado pero se obiara si no esta instalado
+#echo "Securely shredding ${currentscript}"; shred -u ${currentscript};
+echo "Securely shredding ${currentscript}";
+sleep 3
+}
+
+# When your script is finished, exit with a call to the function, "finish":
+trap finish EXIT
+rm --  "$( readlink -f -- "${BASH_SOURCE[0]:-$0}" 2> '/dev/null'; )";
+EOF
+
+# ---------------------- ejemplo N 3
+# :::::: sobreescribe
+cat << EOF >> file.txt
+The current working directory is: $PWD
+You are logged in as $(whoami)
 EOF
 ```
 #### OPCION: 2 - esta no falla hasta la fecha
