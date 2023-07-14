87484271f53d9a7995e7b3db08f867b40b5cab9b
Cesar Auris <perucaos@gmail.com.com>
2023-07-14 16:44:44 -0500

    |  se actualizo rama master :2023-07-14_16:44:39

--- utilidades/Crear_loading_pre_cargador/5_precargador_uso.sh
+++ utilidades/Crear_loading_pre_cargador/5_precargador_uso.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 # 1. Create ProgressBar function
 # 1.1 Input is currentState($1) and totalState($2)
+
+#######################################
+# @description Progresab para usarlo de maneja secuencial
+# @author Cesar Auris
+# @param str
+# @return void
+# @example
+# @since   2023-28-14
+#######################################
+
+
 function ProgressBar {
 # Process data
     let _progress=(${1}*100/${2}*100)/100
@@ -13,19 +24,16 @@ function ProgressBar {
 # 1.2 Build progressbar strings and print the ProgressBar line
 # 1.2.1 Output example:
 # 1.2.1.1 Progress : [########################################] 100%
-printf "\rProgress : [${_fill// /#}${_empty// /-}] ${_progress}%%"
-
+#printf "\rProgress : [${_fill// /#}${_empty// /-}] ${_progress}%%"
+printf "\r[${_fill// /#}${_empty// /-}] ${_progress}%% | ${3}"
 }
 
 end_number=100
-echo -ne 'ya\r'
-ProgressBar 10 ${end_number}  && sleep 4
-echo -ne '\rya'
-ProgressBar 20 ${end_number}  && sleep 3
-echo -ne '\rya'
-ProgressBar 40 ${end_number}  && sleep 4
-echo "subiendo 2 .."
-ProgressBar 80 ${end_number}  && sleep 4
-echo "subiendo 4 .."
-ProgressBar 100 ${end_number}  && sleep 4
+
+ProgressBar 10 ${end_number}  " iniciando copia.." && sleep 2
+ProgressBar 20 ${end_number}  " iniciando copia 2.." && sleep 2
+ProgressBar 100 ${end_number} " iniciando copia 3.." && sleep 2
+echo ""
+echo "completado2"
+
 
