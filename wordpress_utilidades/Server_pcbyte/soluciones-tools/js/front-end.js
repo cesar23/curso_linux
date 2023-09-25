d75e37d690e1279901640540d478b1339ddc8d79
Cesar Auris PC <perucaos@gmail.com>
2023-09-25 09:11:51 -0500

    cesar@$DESKTOP-UDJ9SA0 se actualizo :2023-09-25_09:11:06

--- wordpress_utilidades/Server_pcbyte/soluciones-tools/js/front-end.js
+++ wordpress_utilidades/Server_pcbyte/soluciones-tools/js/front-end.js
@@ -1,39 +1,39 @@
 function htmlUpdatePrecios(soles, dolar) {
 
 
-    dolar = dolar.replace('$', '').replace(',', '');
-    soles = soles.replace('S/.', '').replace(',', '');
-    //-----------------------------------------------
-    //----------------------dolar--------------------
-    //-----------------------------------------------
+  dolar = dolar.replace('$', '').replace(',', '');
+  soles = soles.replace('S/.', '').replace(',', '');
+  //-----------------------------------------------
+  //----------------------dolar--------------------
+  //-----------------------------------------------
 
-    //-- start aplicar aumento de precio  mas el 5%
-    var dolar_add_porcentaje = parseFloat(dolar);
-    dolar_add_porcentaje = dolar_add_porcentaje + (dolar_add_porcentaje * 0.05);
+  //-- start aplicar aumento de precio  mas el 5%
+  var dolar_add_porcentaje = parseFloat(dolar);
+  dolar_add_porcentaje = dolar_add_porcentaje + (dolar_add_porcentaje * 0.05);
 
-    //aplicar formato para  su visualizacion
-    dolar_add_porcentaje = number_format_js(dolar_add_porcentaje, 2);
-    dolar = number_format_js(dolar, 2);
-    //-----------------------------------------------
-    //----------------------soles--------------------
-    //-----------------------------------------------
+  //aplicar formato para  su visualizacion
+  dolar_add_porcentaje = number_format_js(dolar_add_porcentaje, 2);
+  dolar = number_format_js(dolar, 2);
+  //-----------------------------------------------
+  //----------------------soles--------------------
+  //-----------------------------------------------
 
-    //-- start aplicar aumento de precio  mas el 5%
-    var soles_add_porcentaje = parseFloat(soles);
-    soles_add_porcentaje = soles_add_porcentaje + (soles_add_porcentaje * 0.05);
+  //-- start aplicar aumento de precio  mas el 5%
+  var soles_add_porcentaje = parseFloat(soles);
+  soles_add_porcentaje = soles_add_porcentaje + (soles_add_porcentaje * 0.05);
 
-    //aplicar formato para  su visualizacion
-    soles_add_porcentaje = number_format_js(soles_add_porcentaje, 2);
-    soles = number_format_js(soles, 2);
+  //aplicar formato para  su visualizacion
+  soles_add_porcentaje = number_format_js(soles_add_porcentaje, 2);
+  soles = number_format_js(soles, 2);
 
 
-    dolar = humanizeNumber(dolar);
-    dolar_add_porcentaje = humanizeNumber(dolar_add_porcentaje);
+  dolar = humanizeNumber(dolar);
+  dolar_add_porcentaje = humanizeNumber(dolar_add_porcentaje);
 
-    soles = humanizeNumber(soles);
-    soles_add_porcentaje = humanizeNumber(soles_add_porcentaje);
+  soles = humanizeNumber(soles);
+  soles_add_porcentaje = humanizeNumber(soles_add_porcentaje);
 
-    var html = `
+  var html = `
             <div class="tabla_monedas">
                 <ul class="tabla_monedas_list">
                     <li class="moneda_dolares_m">$ ${dolar_add_porcentaje}</li>
@@ -55,130 +55,163 @@ function htmlUpdatePrecios(soles, dolar) {
                 </div>
             </div>
     `;
-    return html;
+  return html;
 }
 
 
 function changeMountStyleListProduct() {
 
-    if (document.querySelector('.woocs_price_info_list')) {
+  if (document.querySelector('.woocs_price_info_list')) {
 
-        document.querySelectorAll(".woocs_price_info").forEach(element => {
+    document.querySelectorAll(".woocs_price_info").forEach(element => {
 
 
-            //var data = element.getAttribute("data-id");
+      //var data = element.getAttribute("data-id");
 
-            //let elemnto_amount=element.querySelector('.woocs_price_info_list');
-            let monto = element.querySelector('.woocs_price_info_list li .woocs_amount').textContent;
+      //let elemnto_amount=element.querySelector('.woocs_price_info_list');
+      let monto = element.querySelector('.woocs_price_info_list li .woocs_amount').textContent;
 
-            element.innerHTML = `<span class="woocommerce-Price-amount amount">
+      element.innerHTML = `<span class="woocommerce-Price-amount amount">
                             <span class="woocommerce-Price-currencySymbol"
                 >${monto}</span></span>`;
 
 
-        });
+    });
 
 
-    }
+  }
 
 
 }
 
 
 function changeMountStyleProduct() {
-    var productPrices = document.querySelector('.price-wrapper');
-    var priceDolar = productPrices.querySelector('.price.product-page-price>.woocs_price_code>span');
-    var priceSoles = productPrices.querySelector('.woocs_price_info>.woocs_price_info_list>li>span.woocs_amount');
-    if(!priceDolar || !priceSoles){
-        return false;
-    }
+  var productPrices = document.querySelector('.price-wrapper');
+  var priceDolar = productPrices.querySelector('.price.product-page-price>.woocs_price_code>span');
+  var priceSoles = productPrices.querySelector('.woocs_price_info>.woocs_price_info_list>li>span.woocs_amount');
+  if (!priceDolar || !priceSoles) {
+    return false;
+  }
 
-    priceDolar = priceDolar.textContent;
-    priceSoles = priceSoles.textContent;
-
-
-
-    var htmlnewPrecios = htmlUpdatePrecios(priceSoles, priceDolar);
-    productPrices.innerHTML = htmlnewPrecios;
+  priceDolar = priceDolar.textContent;
+  priceSoles = priceSoles.textContent;
 
 
-
+  let htmlnewPrecios = htmlUpdatePrecios(priceSoles, priceDolar);
+  productPrices.innerHTML = htmlnewPrecios;
 
 
 //----------- Productos del pie de pagina
 
-    if (document.querySelectorAll('.product_list_widget .woocs_price_info')) {
+  if (document.querySelectorAll('.product_list_widget .woocs_price_info')) {
 
-        document.querySelectorAll(".product_list_widget .woocs_price_info").forEach(element => {
+    document.querySelectorAll(".product_list_widget .woocs_price_info").forEach(element => {
 
 
-            //var data = element.getAttribute("data-id");
+      //var data = element.getAttribute("data-id");
 
-            //let elemnto_amount=element.querySelector('.woocs_price_info_list');
-            let monto = element.querySelector('.woocs_amount').textContent;
+      //let elemnto_amount=element.querySelector('.woocs_price_info_list');
+      let monto = element.querySelector('.woocs_amount').textContent;
 
-            element.innerHTML = `<span class="woocommerce-Price-amount amount">
+      element.innerHTML = `<span class="woocommerce-Price-amount amount">
                             <span class="woocommerce-Price-currencySymbol"
                 >${monto}</span></span>`;
 
 
-        });
+    });
 
 
-    }
+  }
 
 //-------------- Productos relacionados
-    if (document.querySelectorAll('.box-text.box-text-products .woocs_price_info')) {
+  if (document.querySelectorAll('.box-text.box-text-products .woocs_price_info')) {
 
-        document.querySelectorAll(".box-text.box-text-products .woocs_price_info").forEach(element => {
+    document.querySelectorAll(".box-text.box-text-products .woocs_price_info").forEach(element => {
 
-            //var data = element.getAttribute("data-id");
+      //var data = element.getAttribute("data-id");
 
-            //let elemnto_amount=element.querySelector('.woocs_price_info_list');
-            let monto = element.querySelector('.woocs_amount').textContent;
+      //let elemnto_amount=element.querySelector('.woocs_price_info_list');
+      let monto = element.querySelector('.woocs_amount').textContent;
 
-            element.innerHTML = `<span class="woocommerce-Price-amount amount">
+      element.innerHTML = `<span class="woocommerce-Price-amount amount">
                             <span class="woocommerce-Price-currencySymbol"
                 >${monto}</span></span>`;
 
 
-        });
+    });
 
 
-    }
+  }
 
 
 }
 
 function ocultarBotonCompra() {
-    if (document.getElementById('consultar_producto')) {
-        document.querySelectorAll(".cart").forEach(element => {
-            element.style.display = "none";
-        });
-    }
+  if (document.getElementById('consultar_producto')) {
+    document.querySelectorAll(".cart").forEach(element => {
+      element.style.display = "none";
+    });
+  }
 }
 
+
+const ocultarImagenesProductosCategorias = () => {
+  if (document.querySelectorAll('.product-category').length > 0) {
+
+
+    let elements=[]
+    document.querySelectorAll('.product-category').forEach(e => {
+      // e.style.backgroundColor = 'blue'
+      // e.style.display='none'
+      let link=e.querySelector('a').getAttribute('href')
+      let info=e.querySelector('.box-text-inner')
+      let tituloCategoria=info.querySelector('.header-title').textContent || ''
+      let cantidadCategoria=info.querySelector('.count').textContent || ''
+      elements.push(`
+<div class="col-inner">
+<a href="${link}">
+<div class="box box-normal   ">
+<div class="box-text text-center">
+${tituloCategoria} - ${cantidadCategoria}
+</div>
+</div>
+</a>
+</div>
+      `)
+
+      e.remove()
+    })
+ // document.querySelector('.shop-container').innerHTML=elements.join('')
+ // document.querySelector('.shop-container .shop-container').insertAdjacentHTML(elements.join(''))
+    document.querySelector('.shop-container .products.row').insertAdjacentHTML("afterbegin", elements.join(''));
+
+
+  }
+  // window.scrollTo({ top: 2, behavior: 'smooth' });
+  // window.scrollTo({ top: 0, behavior: 'smooth' });
+}
 
 (function () {
 
 
 // paso 1 esperamos que  acrgue la  pagina
-    setTimeout(function () {
-        ocultarBotonCompra();
+  setTimeout(function () {
+    ocultarBotonCompra();
+    ocultarImagenesProductosCategorias()
 
-        // si estamos en la pagina del producto
-        if (document.querySelector('.product-main')) {
-            changeMountStyleProduct();
-            console.log('Mostrar precio al mostrar producto');
+    // si estamos en la pagina del producto
+    if (document.querySelector('.product-main')) {
+      changeMountStyleProduct();
+      console.log('Mostrar precio al mostrar producto');
 
-        } else {
-            // si estamso en el listado de productos
-            changeMountStyleListProduct();
-            console.log('Mostrar precio al listar producto');
-        }
+    } else {
+      // si estamso en el listado de productos
+      changeMountStyleListProduct();
+      console.log('Mostrar precio al listar producto');
+    }
 
 
-    }, 2000);
+  }, 2000);
 
 // Your code here...
 })();
