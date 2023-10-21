af37d3e11e4152f9daa705faa1c52e2c1027ffb1
Cesar Auris PC <perucaos@gmail.com>
2023-10-21 09:17:50 -0500

    cesar@$DESKTOP-UDJ9SA0 se actualizo :2023-10-21_09:17:02

--- Descargar_web_wget/sitio_agama/3_descarga_wget_web.sh
+++ Descargar_web_wget/sitio_agama/3_descarga_wget_web.sh
@@ -1,19 +1,10 @@
 
 declare -a RUTAS_REPOS=(
-"https://grupoagama.com/wp-content/plugins/elementor/assets/lib/font-awesome/webfonts/fa-regular-400.woff"
-"https://grupoagama.com/wp-content/plugins/elementor/assets/lib/font-awesome/webfonts/fa-solid-900.woff"
-"https://grupoagama.com/wp-content/plugins/elementor/assets/lib/font-awesome/webfonts/fa-brands-400.woff"
-"https://grupoagama.com/wp-content/plugins/elementor/assets/lib/font-awesome/webfonts/fa-solid-900.woff"
-"https://grupoagama.com/wp-content/plugins/elementor/assets/lib/font-awesome/webfonts/fa-regular-400.woff"
-"https://grupoagama.com/wp-content/plugins/elementor/assets/lib/font-awesome/webfonts/fa-brands-400.woff"
-"https://grupoagama.com/wp-content/plugins/elementor/assets/lib/font-awesome/webfonts/fa-solid-900.ttf"
-"https://grupoagama.com/wp-content/plugins/elementor/assets/lib/font-awesome/webfonts/fa-regular-400.ttf"
-"https://grupoagama.com/wp-content/plugins/elementor/assets/lib/font-awesome/webfonts/fa-brands-400.ttf"
-"https://grupoagama.com/wp-content/plugins/elementor/assets/lib/eicons/fonts/eicons.woff"
-"https://grupoagama.com/wp-content/plugins/elementor/assets/lib/eicons/fonts/eicons.woff"
-"https://grupoagama.com/wp-content/plugins/elementor/assets/lib/eicons/fonts/eicons.woff"
-"https://grupoagama.com/wp-content/plugins/elementor/assets/lib/eicons/fonts/eicons.ttf"
-"https://grupoagama.com/wp-content/plugins/elementor/assets/lib/eicons/fonts/eicons.ttf"
+"https://grupoagama.com/wp-content/plugins/elementor-pro/assets/js/nav-menu.70d63d6d093f3a45a0c6.bundle.min.js"
+"https://grupoagama.com/wp-content/plugins/elementor-pro/assets/js/animated-headline.ffb4bb4ce1b16b11446d.bundle.min.js"
+"https://grupoagama.com/wp-content/plugins/elementor/assets/js/text-editor.2c35aafbe5bf0e127950.bundle.min.js"
+"https://grupoagama.com/wp-content/plugins/elementor-pro/assets/js/form.72b77b99d67b130634d2.bundle.min.js"
+"https://grupoagama.com/wp-content/plugins/elementor-pro/assets/js/popup.483b906ddaa1af17ff14.bundle.min.js"
 );
 
 for element in "${RUTAS_REPOS[@]}"
